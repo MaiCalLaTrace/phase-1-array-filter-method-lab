be5983e5e669b7229da3c402a0491f89fc058b27
maicallatrace <maicallatrace@gmail.com>
2022-04-06 23:30:50 -0600

    REPO

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(elem => elem.toLowerCase() === string.toLowerCase());
+}
+ function fuzzyMatch(drivers, string) {
+     const match = drivers.filter(function(drivers){
+         
+         if( string === drivers.slice(0,2)){
+             return(drivers);
+         }
+         else {
+             return drivers === string
+     }     
+ })
+ return match
+}
+
+  function matchName(drivers, string) {
+     const driverName = drivers.filter(function(string){
+         for(let object in drivers){
+             if(string === drivers[0]){
+         return drivers
+     }else{
+     return string === drivers[4]
+     }
+    }
+});
+return driverName
+}
